33dff4e9ba4f10f92938974bd48d3e11a12e2518
Dylan <dylan.coben@gmail.com>
2025-01-25 15:28:51 -0500

    Added header and custom styles

--- screens/Feed/FridgeScreen.js
+++ screens/Feed/FridgeScreen.js
@@ -8,6 +8,7 @@ import {
   Platform,
   Alert,
   SafeAreaView,
+  TouchableOpacity,
 } from "react-native";
 import { getBarcodeData } from "../../logic/barcodeFetch";
 import { FridgeContext } from "../../contexts/FridgeContext";
@@ -17,28 +18,18 @@ const FridgeScreen = () => {
   const { fridgeItems, isLoadingFridgeItems, loadFridgeItems, addFridgeItems } =
     useContext(FridgeContext);
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <Text>Header</Text>
+    <SafeAreaView className="flex flex-col">
+      <View className="flex-row justify-between items-center p-4 bg-neo-red">
+        <View className="flex-row justify-between items-center p-4 bg-neo-purple border-black border-[5px] rounded-xl shadow-neo">
+          <Text className="text-3xl font-bold">My Fridge</Text>
+        </View>
+        <View className="flex-row justify-between items-center p-4 bg-neo-purple border-black border-[5px] rounded-xl shadow-neo">
+          <Text className="text-xl font-bold">Sort By: Date</Text>
         </View>
       </View>
+      <TouchableOpacity className="rounded-full bg-neo-green w-20 h-20"></TouchableOpacity>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  header: {
-    fontSize: 24,
-    fontWeight: "bold",
-    margin: 10,
-  },
-});
 
 export default FridgeScreen;
