60c9f0c18b870cc2854baf6fc79f50ddcefa9395
devinwylde <devinwylde@ufl.edu>
2025-01-25 18:54:14 -0500

    added new items screen

--- logic/barcodeFetch.js
+++ logic/barcodeFetch.js
@@ -8,6 +8,16 @@ export const getBarcodeData = async (barcode) => {
         const data = await response.json();
 
         if (!data.status || data.status !== 1) throw new Error('Could not find barcode.');
+
+        let nova_data = null;
+        if (data.product.nova_group && data.product.nova_groups_markers && data.product.nova_groups_markers[data.product.nova_group]) {
+            const nd_list = [];
+            for (const item of data.product.nova_groups_markers[data.product.nova_group]) {
+                if (!item.length) continue;
+                if (item[0] === 'ingredients') nd_list.push(item[1].split(':')[1]);
+            }
+            nova_data = nd_list.join(', ')
+        }
         
         return {
             'id': data.code,
@@ -16,7 +26,13 @@ export const getBarcodeData = async (barcode) => {
             'nutriscore_data': data.product.nutriscore_data,
             'nutriscore_grade': data.product.nutriscore_grade,
             'ecoscore_data': data.product.ecoscore_data,
-            'ecoscore_grade': data.product.ecoscore_grade
+            'ecoscore_grade': data.product.ecoscore_grade,
+            'fat_level': data.product.nutrient_levels.fat,
+            'saturated_fat_level': data.product.nutrient_levels['saturated-fat'],
+            'salt_level': data.product.nutrient_levels.salt,
+            'sugar_level': data.product.nutrient_levels.sugars,
+            'nova_grade': data.product.nova_group,
+            'nova_data': nova_data
         };
     } catch (error) {
         console.error(error.message);
