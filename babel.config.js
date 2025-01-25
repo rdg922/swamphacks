7e1a7fbccf0ae46d541acd033aa974b4b9dcaeef
Rohit Dasgupta <rdg922@gmail.com>
2025-01-25 16:20:06 -0500

    add env

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,13 @@ module.exports = function(api) {
     plugins: [
       'react-native-reanimated/plugin',
       'nativewind/babel',
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
     ],
   };
 };
