b8e1368b7f8eebe548a0cf79687f996076a0c28e
Dylan <dylan.coben@gmail.com>
2025-01-25 14:50:18 -0500

    Added custom nativewind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,32 +3,39 @@ module.exports = {
   content: ["./App.js", "./screens/**/*.js"],
   theme: {
     extend: {
+      colors: {
+        "neo-light-blue": "#7DF9FF",
+        "neo-green": "#FF00F5",
+        "neo-purple": "#FF00F5",
+        "neo-dark-blue": "#3300FF",
+        "neo-yellow": "#FFFF00",
+        "neo-red": "#FF4911",
+      },
       width: {
-        '120p': '120%',
+        "120p": "120%",
       },
       height: {
-        '120p': '120%',
+        "120p": "120%",
       },
       borderColor: {
-        'lilac': '#AE48E2',
+        lilac: "#AE48E2",
       },
       borderWidth: {
-        '0.75': 0.75,
+        0.75: 0.75,
       },
       textColor: {
-        'lilac': '#AE48E2',
+        lilac: "#AE48E2",
       },
       backgroundColor: {
-        'darkblue': '#16032A',
+        darkblue: "#16032A",
       },
       height: {
-        '0.1': 1,
+        0.1: 1,
       },
       width: {
-        '1/10': '10%',
-      }
+        "1/10": "10%",
+      },
     },
   },
   plugins: [],
-}
-
+};
