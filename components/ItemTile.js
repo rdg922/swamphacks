c653422405a91e81e0e39ca5bbc23641dc5a9519
Dylan <dylan.coben@gmail.com>
2025-01-26 02:34:30 -0500

    Added placeholder image

--- components/ItemTile.js
+++ components/ItemTile.js
@@ -2,21 +2,19 @@ import React from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import { styled } from "nativewind";
 
-const ItemTile = ({
-  item,
-  onClick,
-  onCheckClick,
-  navigation,
-}) => {
-let formattedDate = 'N/A';
-if (item.expiryDate) {
-  const date = new Date(item.expiryDate);
-  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Add 1 because months are zero-based
-const day = date.getDate().toString().padStart(2, '0');
-const year = date.getFullYear().toString().slice(-2); // Get the last two digits of the year
+const ItemTile = ({ item, onClick, onCheckClick, navigation }) => {
+  let formattedDate = "N/A";
+  if (item.expiryDate) {
+    const date = new Date(item.expiryDate);
+    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Add 1 because months are zero-based
+    const day = date.getDate().toString().padStart(2, "0");
+    const year = date.getFullYear().toString().slice(-2); // Get the last two digits of the year
 
- formattedDate = `${month}/${day}/${year}`;
-}
+    formattedDate = `${month}/${day}/${year}`;
+  }
+  const placeholderImage =
+    "https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png";
+
   return (
     <TouchableOpacity
       onPress={() =>
@@ -26,9 +24,8 @@ const year = date.getFullYear().toString().slice(-2); // Get the last two digits
     >
       <View className=" rounded-b-xl rounded-t-lg p-4 bg-white">
         <Image
-          source={{ uri: item.image_url }}
-          className="w-full h-40 rounded-lg "
-          resizeMode="contain"
+          source={{ uri: item.image_url || placeholderImage }}
+          className="w-full h-40 rounded-lg"
         />
         <TouchableOpacity
           className={`absolute top-2 right-2 w-8 h-8 rounded-lg border-[3px] ${
