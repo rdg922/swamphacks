59dab99e354623d39b71c810eab612637227639c
devinwylde <devinwylde@ufl.edu>
2025-01-26 01:24:15 -0500

    only pass in item to tile, no split

--- components/ItemTile.js
+++ components/ItemTile.js
@@ -4,25 +4,20 @@ import { styled } from "nativewind";
 
 const ItemTile = ({
   item,
-  id,
-  imageUrl,
-  name,
-  expirationDate,
-  isChecked,
   onClick,
   onCheckClick,
   navigation,
 }) => {
   return (
     <TouchableOpacity
-      onPress={() => navigation.navigate("Item", { barcode: id, owned: true, item })}
+      onPress={() => navigation.navigate("Item", { barcode: item.id, owned: true, item })}
       className="rounded-xl shadow-neo border-black border-[5px] relative bg-neo-light-green w-[48%] mr-1 mb-1"
     >
       <View className=" rounded-b-xl rounded-t-lg p-4 bg-white">
-        <Image source={{ uri: imageUrl }} className="w-full h-40 rounded-lg " />
+        <Image source={{ uri: item.image_url }} className="w-full h-40 rounded-lg " />
         <TouchableOpacity
           className={`absolute top-2 right-2 w-8 h-8 rounded-lg border-[3px] ${
-            isChecked
+            item.isChecked
               ? "bg-neo-green shadow-neo-small"
               : "bg-[#CCCCCC] border-[#999999]"
           }`}
@@ -31,9 +26,9 @@ const ItemTile = ({
         />
       </View>
       <View className=" p-2 px-4 mb-2 bg-neo-light-green rounded-xl">
-        <Text className="text-xl font-bold">{name}</Text>
+        <Text className="text-xl font-bold">{item.name}</Text>
         <Text className="text-sm text-gray-600 font-medium">
-          Expires: {expirationDate}
+          Expires: {item.expirationDate}
         </Text>
       </View>
     </TouchableOpacity>
