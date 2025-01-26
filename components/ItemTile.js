cdd1d3295b8bd3cd371dba8debf8fefad84797ab
Rohit Dasgupta <rdg922@gmail.com>
2025-01-26 03:41:02 -0500

    rohit

--- components/ItemTile.js
+++ components/ItemTile.js
@@ -2,15 +2,26 @@ import React from "react";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import { styled } from "nativewind";
 
+
 const ItemTile = ({ item, onClick, onCheckClick, navigation }) => {
   let formattedDate = "N/A";
   if (item.expiryDate) {
     const date = new Date(item.expiryDate);
-    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Add 1 because months are zero-based
-    const day = date.getDate().toString().padStart(2, "0");
-    const year = date.getFullYear().toString().slice(-2); // Get the last two digits of the year
+    const today = new Date();
+    const next7Days = new Date();
+    next7Days.setDate(today.getDate() + 7);
 
-    formattedDate = `${month}/${day}/${year}`;
+    // Check if the date is within the next 7 days
+    if (date >= today && date <= next7Days) {
+      const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" }); // e.g., "Monday"
+      formattedDate = dayOfWeek;
+    } else {
+      const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Add 1 because months are zero-based
+      const day = date.getDate().toString().padStart(2, "0");
+      const year = date.getFullYear().toString().slice(-2); // Get the last two digits of the year
+
+      formattedDate = `${month}/${day}/${year}`;
+    }
   }
   const placeholderImage =
     "https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png";
@@ -28,11 +39,10 @@ const ItemTile = ({ item, onClick, onCheckClick, navigation }) => {
           className="w-full h-40 rounded-lg"
         />
         <TouchableOpacity
-          className={`absolute top-2 right-2 w-8 h-8 rounded-lg border-[3px] ${
-            item.isChecked
-              ? "bg-neo-green shadow-neo-small"
-              : "bg-[#CCCCCC] border-[#999999]"
-          }`}
+          className={`absolute top-2 right-2 w-8 h-8 rounded-lg border-[3px] ${item.isChecked
+            ? "bg-neo-green shadow-neo-small"
+            : "bg-[#CCCCCC] border-[#999999]"
+            }`}
           hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}
           onPress={onCheckClick}
         />
