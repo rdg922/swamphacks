3aa5203e48bad46c41e6a7983f40467f6dd8eccf
Dylan <dylan.coben@gmail.com>
2025-01-25 23:57:44 -0500

    Added trash button

---
+++ components/TrashButton.js
@@ -0,0 +1,16 @@
+import { FontAwesome6 } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+
+export const TrashButton = ({ onPress }) => {
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        onPress();
+      }}
+      activeOpacity={0.8}
+      className="rounded-full bg-neo-red shadow-neo border-black border-[5px] w-24 h-24 justify-center items-center"
+    >
+      <FontAwesome6 name="trash" color="black" size={40} />
+    </TouchableOpacity>
+  );
+};
